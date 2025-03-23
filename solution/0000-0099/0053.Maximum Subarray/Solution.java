fc56c8a4f715a87722b1b45519be1c4b895fe508
guowei.liu <guowei.liu@foxmail.com>
2025-03-23 23:51:52 +0800

    Update Solution.java
    add easy solution

--- solution/0000-0099/0053.Maximum Subarray/Solution.java
+++ solution/0000-0099/0053.Maximum Subarray/Solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int ans = nums[0];
-        for (int i = 1, f = nums[0]; i < nums.length; ++i) {
-            f = Math.max(f, 0) + nums[i];
-            ans = Math.max(ans, f);
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
         }
-        return ans;
+        return maxAns;
     }
 }
